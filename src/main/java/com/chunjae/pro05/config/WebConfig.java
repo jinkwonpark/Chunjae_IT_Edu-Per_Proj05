ce539c40cc56cb7197c12fd649cff2fdd8d46ec4
jinkwonpark <jk3473@naver.com>
2023-11-10 20:25:59 +0900

    a

--- src/main/java/com/chunjae/pro05/config/WebConfig.java
+++ src/main/java/com/chunjae/pro05/config/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
         registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
